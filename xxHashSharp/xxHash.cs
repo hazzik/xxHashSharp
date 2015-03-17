d2148199a70b27b04ec44e5f5ccab5f821c6324a
Alexander Zaytsev <hazzik@gmail.com>
2015-03-18 01:45:56 +1300

    Implement HashAlgorithm

--- xxHashSharp/xxHash.cs
+++ xxHashSharp/xxHash.cs
@@ -29,16 +29,13 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace xxHashSharp
 {
-    public class xxHash
+    public class xxHash : HashAlgorithm
     {
-        public struct XXH_State
+        private struct XXH_State
         {
             public ulong total_len;
             public uint seed;
@@ -50,81 +47,27 @@ namespace xxHashSharp
             public byte[] memory;
         };
 
-        const uint PRIME32_1 = 2654435761U;
-        const uint PRIME32_2 = 2246822519U;
-        const uint PRIME32_3 = 3266489917U;
-        const uint PRIME32_4 = 668265263U;
-        const uint PRIME32_5 = 374761393U;
+        private const uint PRIME32_1 = 2654435761U;
+        private const uint PRIME32_2 = 2246822519U;
+        private const uint PRIME32_3 = 3266489917U;
+        private const uint PRIME32_4 = 668265263U;
+        private const uint PRIME32_5 = 374761393U;
 
-        protected XXH_State _state;
-        public xxHash()
+        private XXH_State _state;
+
+        private readonly uint seed;
+
+        public xxHash() : this(0)
         {
-
         }
 
-        public static uint CalculateHash(byte[] buf, int len = -1, uint seed = 0)
+        public xxHash(uint seed)
         {
-            uint h32;
-            int index = 0;
-            if (len == -1)
-            {
-                len = buf.Length;
-            }
-
-
-            if (len >= 16)
-            {
-                int limit = len - 16;
-                uint v1 = seed + PRIME32_1 + PRIME32_2;
-                uint v2 = seed + PRIME32_2;
-                uint v3 = seed + 0;
-                uint v4 = seed - PRIME32_1;
-
-                do
-                {
-                    v1 = CalcSubHash(v1, buf, index);
-                    index += 4;
-                    v2 = CalcSubHash(v2, buf, index);
-                    index += 4;
-                    v3 = CalcSubHash(v3, buf, index);
-                    index += 4;
-                    v4 = CalcSubHash(v4, buf, index);
-                    index += 4;
-                } while (index <= limit);
-
-                h32 = RotateLeft(v1, 1) + RotateLeft(v2, 7) + RotateLeft(v3, 12) + RotateLeft(v4, 18);
-            }
-            else
-            {
-                h32 = seed + PRIME32_5;
-            }
-
-            h32 += (uint)len;
-
-            while (index <= len - 4)
-            {
-                h32 += BitConverter.ToUInt32(buf, index) * PRIME32_3;
-                h32 = RotateLeft(h32, 17) * PRIME32_4;
-                index += 4;
-            }
-
-            while(index<len)
-            {
-                h32 += buf[index] * PRIME32_5;
-                h32 = RotateLeft(h32, 11) * PRIME32_1;
-                index++;
-            }
-
-            h32 ^= h32 >> 15;
-            h32 *= PRIME32_2;
-            h32 ^= h32 >> 13;
-            h32 *= PRIME32_3;
-            h32 ^= h32 >> 16;
-
-            return h32;
+            this.seed = seed;
+            Initialize();
         }
 
-        public void Init(uint seed = 0)
+        public override void Initialize()
         {
             _state.seed = seed;
             _state.v1 = seed + PRIME32_1 + PRIME32_2;
@@ -136,40 +79,38 @@ namespace xxHashSharp
             _state.memory = new byte[16];
         }
 
-        public bool Update(byte[] input, int len)
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
         {
-            int index = 0;
+            int index = ibStart;
 
-            _state.total_len += (uint)len;
+            _state.total_len += (uint)cbSize;
 
-            if (_state.memsize + len < 16) // 버퍼 + 입력길이가 16바이트 이하일경우 버퍼에 저장만 해둔다
+            if (_state.memsize + cbSize < 16) // 버퍼 + 입력길이가 16바이트 이하일경우 버퍼에 저장만 해둔다
             {
-                Array.Copy(input, 0, _state.memory, _state.memsize, len);
-                _state.memsize += len;
-
-                return true;
+                Buffer.BlockCopy(array, ibStart, _state.memory, _state.memsize, cbSize);
+                _state.memsize += cbSize;
+                return;
             }
 
             if (_state.memsize > 0) // 이전데이터가 남아있을경우 먼저 처리한다.
             {
-                Array.Copy(input, 0, _state.memory, _state.memsize, 16 - _state.memsize);
+                Buffer.BlockCopy(array, ibStart, _state.memory, _state.memsize, 16 - _state.memsize);
 
-                _state.v1 = CalcSubHash(_state.v1, _state.memory, index);
+                _state.v1 = CalculateSubHash(_state.v1, _state.memory, index);
                 index += 4;
-                _state.v2 = CalcSubHash(_state.v2, _state.memory, index);
+                _state.v2 = CalculateSubHash(_state.v2, _state.memory, index);
                 index += 4;
-                _state.v3 = CalcSubHash(_state.v3, _state.memory, index);
+                _state.v3 = CalculateSubHash(_state.v3, _state.memory, index);
                 index += 4;
-                _state.v4 = CalcSubHash(_state.v4, _state.memory, index);
-                index += 4;
+                _state.v4 = CalculateSubHash(_state.v4, _state.memory, index);
 
                 index = 0;
                 _state.memsize = 0;
             }
 
-            if (index <= len - 16)
+            if (index <= cbSize - 16)
             {
-                int limit = len - 16;
+                int limit = cbSize - 16;
                 uint v1 = _state.v1;
                 uint v2 = _state.v2;
                 uint v3 = _state.v3;
@@ -177,15 +118,16 @@ namespace xxHashSharp
 
                 do
                 {
-                    v1 = CalcSubHash(v1, input, index);
+                    v1 = CalculateSubHash(v1, array, index);
                     index += 4;
-                    v2 = CalcSubHash(v2, input, index);
+                    v2 = CalculateSubHash(v2, array, index);
                     index += 4;
-                    v3 = CalcSubHash(v3, input, index);
+                    v3 = CalculateSubHash(v3, array, index);
                     index += 4;
-                    v4 = CalcSubHash(v4, input, index);
+                    v4 = CalculateSubHash(v4, array, index);
                     index += 4;
-                } while (index <= limit);
+                }
+                while (index <= limit);
 
                 _state.v1 = v1;
                 _state.v2 = v2;
@@ -193,15 +135,14 @@ namespace xxHashSharp
                 _state.v4 = v4;
             }
 
-            if (index < len)
+            if (index < cbSize)
             {
-                Array.Copy(input, index, _state.memory, 0, len - index);
-                _state.memsize = len - index;
+                Buffer.BlockCopy(array, index, _state.memory, 0, cbSize - index);
+                _state.memsize = cbSize - index;
             }
-            return true;
         }
 
-        public uint Digest()
+        protected override byte[] HashFinal()
         {
             uint h32;
             int index = 0;
@@ -214,19 +155,19 @@ namespace xxHashSharp
                 h32 = _state.seed + PRIME32_5;
             }
 
-            h32 += (UInt32)_state.total_len;
+            h32 += (UInt32) _state.total_len;
 
             while (index <= _state.memsize - 4)
             {
-                h32 += BitConverter.ToUInt32(_state.memory, index) * PRIME32_3;
-                h32 = RotateLeft(h32, 17) * PRIME32_4;
+                h32 += BitConverter.ToUInt32(_state.memory, index)*PRIME32_3;
+                h32 = RotateLeft(h32, 17)*PRIME32_4;
                 index += 4;
             }
 
             while (index < _state.memsize)
             {
-                h32 += _state.memory[index] * PRIME32_5;
-                h32 = RotateLeft(h32, 11) * PRIME32_1;
+                h32 += _state.memory[index]*PRIME32_5;
+                h32 = RotateLeft(h32, 11)*PRIME32_1;
                 index++;
             }
 
@@ -236,12 +177,13 @@ namespace xxHashSharp
             h32 *= PRIME32_3;
             h32 ^= h32 >> 16;
 
-            return h32;
+            return BitConverter.GetBytes(h32);
         }
-        private static uint CalcSubHash(uint value, byte[] buf, int index)
+
+        private static uint CalculateSubHash(uint value, byte[] bufer, int index)
         {
-            uint read_value = BitConverter.ToUInt32(buf, index);
-            value += read_value * PRIME32_2;
+            uint v = BitConverter.ToUInt32(bufer, index);
+            value += v*PRIME32_2;
             value = RotateLeft(value, 13);
             value *= PRIME32_1;
             return value;
@@ -251,6 +193,5 @@ namespace xxHashSharp
         {
             return (value << count) | (value >> (32 - count));
         }
-
     }
 }
